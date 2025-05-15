41f8d85787f9cb10dadb3ea213bc7d0bd19a1491
uninstall-your-browser <170106706+uninstall-your-browser@users.noreply.github.com>
2025-05-15 15:50:11 +1000

    Add granite mixer recipe

--- kubejs/server_scripts/diorite.js
+++
@@ -1,4 +0,0 @@
-
-ServerEvents.recipes(event => {
-    event.recipes.create.mixing("3x minecraft:diorite", ["3x minecraft:quartz", "3x minecraft:cobblestone"])
-});
---
+++ kubejs/server_scripts/granite.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.recipes.create.mixing("1x minecraft:granite", ["2x minecraft:quartz", "1x minecraft:cobblestone"])
+});
+
