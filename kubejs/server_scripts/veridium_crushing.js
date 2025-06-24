1c40d3e4956587100dbe6dbbe7a92c7987af785b
uninstall-your-browser <170106706+uninstall-your-browser@users.noreply.github.com>
2025-06-24 14:13:48 +1000

    Fix custom veridium script deleting tuff crushing recipe

--- kubejs/server_scripts/veridium_crushing.js
+++ kubejs/server_scripts/veridium_crushing.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(event => {
-    event.remove({output: [Item.of("create:crushed_raw_copper").withChance(0.8), Item.of("create:copper_nugget").withChance(0.8)]});
+    event.remove({id: "create:crushing/veridium_recycling"});
+    event.remove({id: "create:crushing/veridium"});
     event.recipes.createCrushing(
         [Item.of("create:crushed_raw_copper").withChance(0.1), Item.of("create:copper_nugget").withChance(0.1)],
-        "create:veridium"
+                                 "create:veridium"
     );
 });
